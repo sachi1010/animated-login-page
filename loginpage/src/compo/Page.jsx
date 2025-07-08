9b8454a19a8c7e022bd4608efc31463aad4646ae
sachi1010 <sachisachi8567@gmail.com>
2025-07-08 20:39:38 +0530

    up

--- loginpage/src/compo/Page.jsx
+++ loginpage/src/compo/Page.jsx
@@ -27,7 +27,7 @@ const Page = () => {
     }
 
     try {
-      const res = await fetch('http://localhost:5050/api/user/signup', {
+      const res = await fetch('https://animated-login-page.onrender.com/api/user/signup', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
@@ -52,7 +52,7 @@ const Page = () => {
     const { loginEmail, loginPassword } = formData;
 
     try {
-      const res = await fetch('http://localhost:5050/api/user/login', {
+      const res = await fetch('https://animated-login-page.onrender.com/api/user/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
